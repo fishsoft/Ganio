bc169a1d4e6e0c483d4e030cc0f5fc82b37590af
morse <zm902485jgsurjgc@163.com>
2018-06-27 14:28:57 +0800

    完善fragment

---
+++ app/src/main/java/com/morse/ganio/GanApp.kt
@@ -0,0 +1,16 @@
+package com.morse.ganio
+
+import android.app.Application
+
+class GanApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        app = this
+    }
+
+    companion object {
+        private var app: Application? = null
+        fun getApp() = this.app
+    }
+}
